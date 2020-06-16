376af13022439ddbabeda952a8e6480e7ae80860
Tan Theng Lee <thengleetan@gmail.com>
2020-06-16 11:56:59 +0800

    Add theme context for hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 // @format
 
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("darkblue");
+
   return (
     <React.StrictMode>
-      <div id="something-important">
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="details/:id" />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div id="something-important">
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
