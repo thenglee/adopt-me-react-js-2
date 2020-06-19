de02b93547a594d27dfe7605d9803bca3b99d6e5
Tan Theng Lee <thengleetan@gmail.com>
2020-06-19 15:47:48 +0800

    Add emotion

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 
 import React, { useState } from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+import NavBar from "./NavBar";
 
 const App = () => {
   const themeHook = useState("darkblue");
@@ -14,9 +15,7 @@ const App = () => {
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
         <div id="something-important">
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
+          <NavBar />
           <Router>
             <SearchParams path="/" />
             <Details path="details/:id" />
---
+++ src/colors.js
@@ -0,0 +1,8 @@
+// @format
+
+export default {
+  primary: "#bf3334",
+  secondary: "#d9c148",
+  dark: "#122622",
+  light: "#81a69b",
+};
